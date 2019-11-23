3cb4cd27efc95c21803380184d81b5d1b656ea5b
Samurakuri <57249289+Samurakuri@users.noreply.github.com>
2019-11-23 19:40:22 +0800

    Update bilibili_user.py

--- bilibili_user.py
+++ bilibili_user.py
@@ -29,17 +29,7 @@ def LoadUserAgents(uafile):
     return uas
 
 
-uas = LoadUserAgents("user_agents.txt")
-head = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Referer': 'http://space.bilibili.com/45388',
-    'Origin': 'http://space.bilibili.com',
-    'Host': 'space.bilibili.com',
-    'AlexaToolbar-ALX_NS_PH': 'AlexaToolbar/alx-4.0',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4',
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-}
+
 
 # Please replace your own proxies.
 proxies = {
